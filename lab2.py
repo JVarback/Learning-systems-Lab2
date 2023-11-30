cbc2a736f7c3e5aa88d58e7081ae66cb918d8ad2
Jonatan Varbäck <j.varback00@gmail.com>
2023-11-30 15:48:52 +0100

    Init functions and creating individuals

--- lab2.py
+++ lab2.py
@@ -1,5 +1,26 @@
+import random
 
-def hej():
+# Hyperparameters
+locations = 52
+
+# Skapar en random individ med alla locations i en lista
+def random_individual(locations): 
+    indv = random.sample(range(1,locations+1), locations-1)
+    return indv
+
+def fitness():
     pass
 
-input('hej')
+def crossover():
+    pass
+
+def mutation():
+    pass
+
+def selection():
+    pass
+
+
+
+individual = random_individual(locations)
+print(individual)
