0dd203da8a0e7f53ef14e10b6e3ac0bb886ce031
Jonatan Varbäck <j.varback00@gmail.com>
2023-11-30 16:54:44 +0100

    Fixed data handling problem

--- lab2.py
+++ lab2.py
@@ -1,5 +1,7 @@
 import random
 import re
+import math
+import numpy as np
 
 # Data prep
 with open('berlin52.tsp', 'r') as file:
@@ -7,9 +9,10 @@ with open('berlin52.tsp', 'r') as file:
 
 read_start = lines.index('NODE_COORD_SECTION\n') + 1
 
-data_list = [re.findall(r'\d+', line) for line in lines[read_start:] if line.strip()]
+data_list = [re.findall(r'\d+\.\d+|\d+', line) for line in lines[read_start:] if line.strip()]
 data_list = [data for data in data_list if len(data) >= 3]
 data_list = [(int(data[0]), float(data[1]), float(data[2])) for data in data_list]
+
 
 
 # Hyperparameters
@@ -19,6 +22,14 @@ locations = 52
 def random_individual(locations): 
     indv = random.sample(range(1,locations+1), locations-1)
     return indv
+
+# Calculates the distance between two places
+def distance_calc(coord1, coord2):
+    x1, y1 = coord1[1], coord1[2]
+    x2, y2 = coord2[1], coord2[2]
+
+    distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    return distance
 
 def fitness():
     pass
@@ -35,4 +46,5 @@ def selection():
 
 
 individual = random_individual(locations)
-print(individual)
+
+print(distance_calc(data_list[0], data_list[1]))
