736b5600a7292a5b79b8f6c786b0c40e7c28f436
Jonatan Varbäck <j.varback00@gmail.com>
2023-11-30 16:12:38 +0100

    Done with data preperations

--- lab2.py
+++ lab2.py
@@ -1,4 +1,16 @@
 import random
+import re
+
+# Data prep
+with open('berlin52.tsp', 'r') as file:
+    lines = file.readlines()
+
+read_start = lines.index('NODE_COORD_SECTION\n') + 1
+
+data_list = [re.findall(r'\d+', line) for line in lines[read_start:] if line.strip()]
+data_list = [data for data in data_list if len(data) >= 3]
+data_list = [(int(data[0]), float(data[1]), float(data[2])) for data in data_list]
+
 
 # Hyperparameters
 locations = 52
